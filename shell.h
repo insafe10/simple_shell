f3e36de65ccffaca2b5a364ec6ce141873ec7c5a
insafe10 <essaadyinsafe@gmail.com>
2023-07-22 06:25:07 -0700

    shell file

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define MAX_COMMAND_LENGTH 1024
+
+void execute_command(char *command);
+
+#endif 
